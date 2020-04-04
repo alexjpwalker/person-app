f8b486de1bb214bce8655f798f5acb73c30c70fa
Alex Walker <alexjpwalker@gmail.com>
2020-04-04 10:19:52 +0100

    Create person list page

---
+++ PersonApp/PersonApp/Controllers/ControllerBase.cs
@@ -0,0 +1,16 @@
+﻿using PersonApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PersonApp.Controllers
+{
+    public abstract class BaseController : Controller
+    {
+        protected IPersonAppContext Context => _context;
+
+        private IPersonAppContext _context = PersonAppContext.Instance;
+    }
+}
---
+++ PersonApp/PersonApp/Controllers/PersonController.cs
@@ -0,0 +1,33 @@
+﻿using PersonApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PersonApp.Controllers
+{
+    public class PersonController : BaseController
+    {
+        public async Task<ActionResult> Index()
+        {
+            var people = await Context.People
+                .Include(p => p.PersonGroup)
+                .ToListAsync();
+            return View(people);
+        }
+
+        [Route("{id}")]
+        public ActionResult Details(int id)
+        {
+            var person = Context.People.SingleOrDefault(x => x.Id == id);
+            if (person == null)
+            {
+                return View();
+            }
+            return View(person);
+        }
+    }
+}
