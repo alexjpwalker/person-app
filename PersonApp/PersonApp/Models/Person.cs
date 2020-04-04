52593488c37e558c028367d7849540beb724402e
Alex Walker <alexjpwalker@gmail.com>
2020-04-04 12:28:52 +0100

    Fix circular JSON structure of Person and don't serialize FullName

--- PersonApp/PersonApp/Models/Person.cs
+++ PersonApp/PersonApp/Models/Person.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
@@ -14,10 +15,11 @@ namespace PersonApp.Models
         public int Id { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
+        [JsonIgnore]
         public PersonGroup PersonGroup { get; set; }
         public int PersonGroupId { get; set; }
         public DateTime CreatedDate { get; set; }
-
+        [JsonIgnore]
         public string FullName => $"{FirstName} {LastName}";
     }
 }
