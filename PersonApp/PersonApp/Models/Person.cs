f1c60a14e858f62ce2e81e7f8ca4687e16fcea1e
Alex Walker <alexjpwalker@gmail.com>
2020-04-04 10:16:46 +0100

    Create models

---
+++ PersonApp/PersonApp/Models/Person.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace PersonApp.Models
+{
+    [Table("Person")]
+    public class Person
+    {
+        [Key]
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public PersonGroup PersonGroup { get; set; }
+        public int PersonGroupId { get; set; }
+        public DateTime CreatedDate { get; set; }
+    }
+}
---
+++ PersonApp/PersonApp/Models/PersonGroup.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace PersonApp.Models
+{
+    [Table("PersonGroup")]
+    public class PersonGroup
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public List<Person> People { get; set; }
+    }
+}
