8df4efe90c16141eaabe229a63f87aff08df9e35
Alex Walker <alexjpwalker@gmail.com>
2020-04-04 12:28:25 +0100

    Use JSON instead of XML for API serialization

--- PersonApp/PersonApp/App_Start/WebApiConfig.cs
+++ PersonApp/PersonApp/App_Start/WebApiConfig.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿using Newtonsoft.Json.Serialization;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http.Formatting;
+using System.Net.Http.Headers;
 using System.Web;
 using System.Web.Http;
 
@@ -10,10 +13,20 @@ namespace PersonApp
     {
         public static void Register(HttpConfiguration config)
         {
+            // Make JSON the default WebAPI serialization format
+            config.Formatters.JsonFormatter.MediaTypeMappings.Add(new RequestHeaderMapping(
+                "Accept",
+                "text/html",
+                StringComparison.InvariantCultureIgnoreCase,
+                true,
+                "application/json"));
+            // Use camelCase to match JavaScript / JSON convention
+            config.Formatters.JsonFormatter.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+
             // Enable attribute routing
             config.MapHttpAttributeRoutes();
 
-            // Standard route syntax
+            // Use standard API route syntax
             config.Routes.MapHttpRoute(
                 name: "DefaultApi",
                 routeTemplate: "api/{controller}/{id}",
