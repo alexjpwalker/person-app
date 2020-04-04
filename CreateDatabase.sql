b0301f66800a3559b0d513d58bcec5d089ed261e
Alex Walker <alexjpwalker@gmail.com>
2020-04-04 09:50:09 +0100

    Make inserting People independent of the autogenerated Group IDs

--- CreateDatabase.sql
+++ CreateDatabase.sql
@@ -1,6 +1,7 @@
 DROP DATABASE IF EXISTS CrunchyFrog
 GO
 
+/* Memorable database name. */
 /* All object names are in PascalCase by SQL Server convention. */
 CREATE DATABASE [CrunchyFrog]
  CONTAINMENT = NONE
@@ -63,11 +64,17 @@ INSERT INTO dbo.PersonGroup (Name)
 VALUES ('Group 1'), ('Group 2'), ('Group 3')
 GO
 
+/* Select IDs by name so that inserting People is independent of the auto-generated primary keys */
+DECLARE @g1 INT = (SELECT Id FROM dbo.PersonGroup WHERE Name = 'Group 1')
+DECLARE @g2 INT = (SELECT Id FROM dbo.PersonGroup WHERE Name = 'Group 2')
+DECLARE @g3 INT = (SELECT Id FROM dbo.PersonGroup WHERE Name = 'Group 3')
+
+/* My personal preference is to make the VALUES line up */
 INSERT INTO dbo.Person (FirstName, LastName, PersonGroupId)
-VALUES ('Graham', 'Chapman', 1),
-       ('John', 'Cleese', 2),
-	   ('Terry', 'Gilliam', 3),
-	   ('Terry', 'Jones', 1),
-	   ('Eric', 'Idle', 2),
-	   ('Michael', 'Palin', 2)
+VALUES ('Graham', 'Chapman', @g1),
+       ('John', 'Cleese', @g2),
+	   ('Terry', 'Gilliam', @g3),
+	   ('Terry', 'Jones', @g1),
+	   ('Eric', 'Idle', @g2),
+	   ('Michael', 'Palin', @g2)
 GO
--- PersonApp/Database/PostDeployment/PopulateDatabase.sql
+++ PersonApp/Database/PostDeployment/PopulateDatabase.sql
@@ -2,12 +2,17 @@
 VALUES ('Group 1'), ('Group 2'), ('Group 3')
 GO
 
-/* My personal preference is to make the VALUES line up. */
+/* Select IDs by name so that inserting People is independent of the auto-generated primary keys */
+DECLARE @g1 INT = (SELECT Id FROM dbo.PersonGroup WHERE Name = 'Group 1')
+DECLARE @g2 INT = (SELECT Id FROM dbo.PersonGroup WHERE Name = 'Group 2')
+DECLARE @g3 INT = (SELECT Id FROM dbo.PersonGroup WHERE Name = 'Group 3')
+
+/* My personal preference is to make the VALUES line up */
 INSERT INTO dbo.Person (FirstName, LastName, PersonGroupId)
-VALUES ('Graham', 'Chapman', 1),
-       ('John', 'Cleese', 2),
-	   ('Terry', 'Gilliam', 3),
-	   ('Terry', 'Jones', 1),
-	   ('Eric', 'Idle', 2),
-	   ('Michael', 'Palin', 2)
+VALUES ('Graham', 'Chapman', @g1),
+       ('John', 'Cleese', @g2),
+	   ('Terry', 'Gilliam', @g3),
+	   ('Terry', 'Jones', @g1),
+	   ('Eric', 'Idle', @g2),
+	   ('Michael', 'Palin', @g2)
 GO
