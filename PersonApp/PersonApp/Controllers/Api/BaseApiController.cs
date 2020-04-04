c63040c60c61f53faef1c9fe6decaf88d921ebfa
Alex Walker <alexjpwalker@gmail.com>
2020-04-04 12:28:11 +0100

    Add Person API controller

---
+++ PersonApp/PersonApp/Controllers/Api/BaseApiController.cs
@@ -0,0 +1,17 @@
+﻿using PersonApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Mvc;
+
+namespace PersonApp.Controllers.Api
+{
+    public abstract class BaseApiController : ApiController
+    {
+        protected IPersonAppContext Context => _context;
+
+        private IPersonAppContext _context = PersonAppContext.Instance;
+    }
+}
---
+++ PersonApp/PersonApp/Controllers/Api/PeopleController.cs
@@ -0,0 +1,38 @@
+﻿using PersonApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace PersonApp.Controllers.Api
+{
+    public class PeopleController : BaseApiController
+    {
+        // GET api/people
+        [HttpGet]
+        public async Task<List<Person>> Get()
+        {
+            var people = await Context.People.ToListAsync();
+            return people;
+        }
+
+        // GET api/people/7
+        [HttpGet]
+        public async Task<Person> Get(int id)
+        {
+            var person = await Context.People.SingleOrDefaultAsync(x => x.Id == id);
+            return person;
+        }
+
+        // POST api/people
+        [HttpPost]
+        public Person Post([FromBody] Person person)
+        {
+            return Context.People.Add(person);
+        }
+    }
+}
